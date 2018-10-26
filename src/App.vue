3f9b702de1ffa12a1dcd5f80d619b59b58e54920
Ignacio Guerra <ignacio@igguerra.com.ar>
2018-10-25 23:35:14 -0300

    Webfont

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </script>
 
 <style lang="scss">
+
   @import '../node_modules/bootstrap/dist/css/bootstrap.css';
   @import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
 
--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,15 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]'),
+          publicPath: process.env.NODE_ENV === 'production' ? '../../' : '/'
+        }
       }
     ]
   },
